b679ffeabd708184c5a2ca185c28e5b576ab2d25
Armin <Armin@DESKTOP-LVFB4U7>
2022-01-18 12:23:33 +0100

    Add project files.

---
+++ FIZZ BUZZ 3/Program.cs
@@ -0,0 +1,21 @@
+﻿int i = 1;
+while ( i <= 100 )
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine(i + (" FIZZ BUZZ"));
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine(i + (" FIZZ"));
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(i + (" BUZZ"));
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
